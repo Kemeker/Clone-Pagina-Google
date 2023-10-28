07eb768a47c9d9be566d7de0d26fee16faa3e874
Kemeker <kemeker@gmail.com>
2023-10-27 21:38:28 -0300

    primeiros testes da pagina, criado input

---
+++ clone-google/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="input-group">
+            <input type="text" className="form-control" placeholder="Pesquisar..." aria-label="Pesquisar" aria-describedby="basic-addon2" />
+            
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+    
+  );
+}
+
+export default App;
