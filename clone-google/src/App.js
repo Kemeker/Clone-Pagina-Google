a0a2fb892af1e87a0a47758a20e58e80137830a8
Kemeker <kemeker@gmail.com>
2023-10-27 22:02:05 -0300

    adicionado logo do google, precisa ajustar tamanho

--- clone-google/src/App.js
+++ clone-google/src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
+import React from 'react'
+import logo from '../src/assets/logo.png'
+
 import './App.css';
+
 
 function App() {
   return (
@@ -7,6 +10,7 @@ function App() {
       <div className="row justify-content-center">
         <div className="col-md-6">
           <div className="input-group">
+            <img src={logo} alt="logo"/>
             <input type="text" className="form-control" placeholder="Pesquisar..." aria-label="Pesquisar" aria-describedby="basic-addon2" />
             
           </div>
