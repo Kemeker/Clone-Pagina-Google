5539aff100dbe4ddedf14a4669e77594054a84c9
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-10-30 17:03:26 -0300

    implementado componente SearchBar.js adicionado a pagina e testado ok

--- clone-google/src/App.js
+++ clone-google/src/App.js
@@ -1,40 +1,15 @@
 import React, {useState} from 'react';
-import logo from '../src/assets/logo.png'
+
 
 function App(){
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState('Digite algo no campo de pesquisa')
  
   const handleSearch = () => {
     // implementar a logica 
     alert('Voce esta pesquisando:  ' + query)
   }
   return(
-    <div className="container mt-4">
-    <div className="row justify-content-center">
-      <div className="col-md-6">
-        <div className="input-group">
-          
-          <img src="{logo}" alt="logo" />
-          <input
-            type="text"
-            placeholder="Digite sua pesquisa...."
-            onChange={(e) => setQuery(e.target.value)}
-            className="form-control"
-            aria-label="Pesquisar" 
-            aria-describedby="basic-addon2"
-          />
-          <button onClick={handleSearch}>Pesquisa</button>
- 
-          
-        </div>
-      </div>
-
-    </div>
-
-  </div>
+    <h1>Google</h1>
   )
 }
 export default App
-
-// a funçao do botao handleSearch esta diretamente em App.js para teste
-// porem vai ser testada como um componente e chamada na pagina principal
--- clone-google/src/components/SearchBar.js
+++ clone-google/src/components/SearchBar.js
@@ -1,26 +1,41 @@
 import React, { useState } from 'react'
+import logo from '../src/assets/logo'
 
 
 function SearchBar () {
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState('Digite algo no campo')
 
   const handleSearch = () => {
     // implementar a logica 
-    alert('Voce esta procurando: ${query}')
+    alert('Voce esta procurando: ' + query)
   }
   return(
-    <div className="input-group">
-          <img src="{logo}" alt="logo" />
-          <input
-            type="text"
-            placeholder="Digite sua pesquisa...."
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <button onClick={handleSearch}>Pesquisa</button>
+    <div className="container mt-4">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="input-group">
+          
+              <img src="{logo}" alt="logo" />
+              <input
+                type="text"
+                placeholder="Digite sua pesquisa...."
+                onChange={(e) => setQuery(e.target.value)}
+                className="form-control"
+                aria-label="Pesquisar" 
+                aria-describedby="basic-addon2"
+              />
+            <button onClick={handleSearch}>Pesquisa</button>
+ 
+          
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
 
 export default SearchBar
 
-// em teste barra de pesquisas separada do App.js como um componente
+// Input com botao implementado atraves de componente
