57bf120a62458f2703948d055a50f0b779ba6858
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-10-31 17:00:21 -0300

    alinhado intens a direita da pagina

--- clone-google/src/components/Menu.js
+++ clone-google/src/components/Menu.js
@@ -4,7 +4,7 @@ import '../style/Menu.css'
 
 function Menu (){
   return(
-    <div className="menu">
+    <div className="d-flex justify-content-end">
       <a href="https://mail.google.com/" className="mx-2">Gmail</a>
       <a href="https://www.google.com.br/imghp?hl=pt-BR&ogbl" className="mx-2">Imagens</a>
     </div> 
