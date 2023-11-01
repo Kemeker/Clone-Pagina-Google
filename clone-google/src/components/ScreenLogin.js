732bd84189f126dd74787080c8ad23bc3910239e
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-11-01 15:20:48 -0300

    atualizaçao do menu, as fotos e o loginn vao ser criados

---
+++ clone-google/src/components/ScreenLogin.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect } from 'react'
+import { KeyboardAvoidingView, TextInput, TouchableOpacity, Image, Text, View} from 'react-native';
+import '../assets/Screenlogin.css'
+
+
+function Login () {
+  return{
+    <h1>Login</h1>
+
+  }
+}
+
+export default Login
