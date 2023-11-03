d2f93fa45b1f2885aa52fc1efa841a24f81a82bf
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-11-03 10:20:21 -0300

    adicionado button azul

--- clone-google/src/components/Menu.js
+++ clone-google/src/components/Menu.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import reactDOM from 'react-dom/client'
 import '../style/Menu.css'
+import {menu} from '../assets/menu.png'
 
 function Menu (){
   return(
     <div className="d-flex justify-content-end">
-      <a href="https://mail.google.com/" className="mx-2">Gmail</a>
-      <a href="https://www.google.com.br/imghp?hl=pt-BR&ogbl" className="mx-2">Imagens</a>
+      <div>
+        <a href="gmail.com.br" className="mx-2">Gmail</a>
+      </div>
+      <div>
+        <a href="https://www.google.com.br/imghp?hl=pt-BR&ogbl" className="mx-2">Imagens</a>
+      </div>
+      <div>
+        <button>
+          <img src="{menu}" classname="img-thumbnail" alt="menu"/>
+        </button>
+      </div>
+      <div>
+        <button className="btn btn-outline-primary conectar">
+          Conectar
+        </button>
+      </div>
+
     </div> 
   )
 }
